38b7c42fdbec20f28f36973e8f8e60653b0799a0
ClaireVanlerberghe <vanlerberghe24@gmail.com>
2023-11-13 14:10:36 +0100

    "Initialisation projet"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClientPageComponent } from './client-page/client-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{ path: '', component: HomePageComponent},
+{ path:'client', component: ClientPageComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ClientPageComponent } from './client-page/client-page.component';
+import { MenuComponent } from './menu/menu.component';
+import { CreateButtonComponent } from './create-button/create-button.component';
+import { CreateModalComponent } from './create-modal/create-modal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    ClientPageComponent,
+    MenuComponent,
+    CreateButtonComponent,
+    CreateModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
